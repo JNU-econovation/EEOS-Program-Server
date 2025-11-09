4a4d9c40887ac6ba72da2e6bc040cfd5c36a9073
Bellmin <whdals011203@naver.com>
2025-11-09 21:59:04 +0900

    chore: 프로젝트 초기 세팅

---
+++ eeos-program/src/main/java/com/eeos/program/EeosProgramApplication.java
@@ -0,0 +1,13 @@
+package com.eeos.program;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EeosProgramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EeosProgramApplication.class, args);
+    }
+
+}
